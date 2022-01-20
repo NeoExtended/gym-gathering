c929a98e37da3eb47dff29201cc1a0068c160a33
Matthias Konitzny <konitzny@ibr.cs.tu-bs.de>
2022-01-20 16:33:41 +0100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="gym_environments",
+    name="gym_gathering",
+    packages=[package for package in find_packages() if package.startswith("gym_gathering")],
     version="1.0.0",
     author="Matthias Konitzny",
-    description="Adds additional environments to the OpenAI Gym package",
-    install_requires=["gym", "numpy", "scipy"],
+    author_email="konitzny@ibr.cs.tu-bs.de",
+    description="A gym-compatible reinforcement learning environment which simulates various particle gathering "
+                "problems",
+    keywords="reinforcement-learning machine-learning gym environment python "
+             "data-science particle-gathering gathering particles algorithms",
+    install_requires=["gym", "numpy", "scipy", "opencv-python"],
     extras_require={"keyboard": ["keyboard"]},
-    include_package_data=True,
-    package_data={"": ["mapdata/*.csv"]},
+#    include_package_data=True,
+    package_data={"gym_gathering": ["mapdata/*.csv"]},
 )
