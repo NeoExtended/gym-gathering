bd21724aea8ac3a4a1e15dabe14eb6495b23a076
Matthias Konitzny <konitzny@ibr.cs.tu-bs.de>
2022-01-27 18:48:52 +0100

    Fixed multiple bugs in the physical particle modifier.

--- gym_gathering/steps/physical_modifier.py
+++ gym_gathering/steps/physical_modifier.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Dict, Tuple
 
 import numpy as np
@@ -9,19 +10,20 @@ class PhysicalMovementModifier(StepModifier):
     def __init__(
         self,
         action_map: Dict[int, Tuple[int, int]],
-        random_weights: bool = True,
+        random_particle_weights: bool = True,
+        max_particle_weight: float = 1.2,
         force: float = 0.05,
         drag: float = 1.025,
         particle_self_collision: bool = True,
         max_particles_per_cell: int = 3,
-        collision_range: int = 3,
         initially_frozen: float = 0.0,
         frozen_state_change_prob: Tuple[float, float] = (0.0, 0.0),
-        **kwargs
+        **kwargs,
     ):
         super(PhysicalMovementModifier, self).__init__(action_map, **kwargs)
 
-        self.random_particle_weights = random_weights
+        self.random_particle_weights = random_particle_weights
+        self.max_particle_weight = max_particle_weight
         self.particle_speed = None
         self.exact_locations = None
         self.force = force
@@ -31,7 +33,6 @@ class PhysicalMovementModifier(StepModifier):
         self.max_particles_per_cell = max_particles_per_cell
         self.use_self_collision = particle_self_collision
         self.collisions = np.ndarray([])
-        self.collision_range = collision_range
 
         self.initially_frozen = initially_frozen
         self.frozen_state_change_prob = frozen_state_change_prob
@@ -39,15 +40,16 @@ class PhysicalMovementModifier(StepModifier):
 
     def reset(self, locations: np.ndarray, maze: np.ndarray, freespace: np.ndarray):
         super(PhysicalMovementModifier, self).reset(locations, maze, freespace)
-        self.exact_locations = np.copy(locations)
 
         n_particles = len(locations)
         self.frozen = self.np_random.uniform(size=n_particles) <= self.initially_frozen
 
         self.particle_speed = np.zeros((n_particles, 2))
-        self.exact_locations = np.zeros((n_particles, 2))
+        self.exact_locations = np.copy(locations)
         if self.random_particle_weights:
-            self.particle_weight = self.np_random.randint(1, 2, size=(n_particles,))
+            self.particle_weight = (
+                self.max_particle_weight - 1
+            ) * self.np_random.random(size=(n_particles,)) + 1
         else:
             self.particle_weight = np.full((n_particles,), 1)
 
@@ -81,8 +83,11 @@ class PhysicalMovementModifier(StepModifier):
         # Stop the movement of frozen particles
         self.particle_speed *= ~self.frozen[:, np.newaxis]
 
+        # Calculate exact positions
+        exact_locations = self.exact_locations + self.particle_speed
+
         # Calculate integer update
-        rounded_update = np.rint(self.particle_speed).astype(int)
+        rounded_update = np.rint(exact_locations).astype(int) - locations
 
         if self.use_self_collision:
             valid_locations = self.self_collision(locations)
@@ -99,32 +104,50 @@ class PhysicalMovementModifier(StepModifier):
         return valid_locations[:, np.newaxis]
 
     def self_collision(self, locations: np.ndarray):
-        # Calculate normed particle direction vectors
-        norm = np.linalg.norm(self.particle_speed, axis=1)
-        # Prevent division by zero on zero length vectors
-        norm[norm == 0.0] = 1.0
+        # Calculate the future position of each particle
+        positions = np.rint(self.exact_locations + self.particle_speed).astype(int)
 
-        directions = self.particle_speed / norm[:, np.newaxis]
+        # Initial collisions are wall collisions
+        collisions = self.check_collision(self.freespace, positions)
+        current_collisions = len(collisions) - collisions.sum()
+        prev_collisions = -1
 
-        # Calculate maze positions that are blocked by walls or other particles
-        unique, counts = np.unique(locations, return_counts=True, axis=0)
-        current_distribution = np.copy(self.maze) * self.max_particles_per_cell
-        current_distribution[unique[:, 0], unique[:, 1]] = counts
-        current_distribution[current_distribution < self.max_particles_per_cell] = 1
-        current_distribution[current_distribution >= self.max_particles_per_cell] = 0
+        # Calculate collisions by iteratively resetting particles which cannot
+        # enter their target pixel to their original position.
+        while current_collisions > prev_collisions:
+            prev_collisions = current_collisions
 
-        # Calculate collisions. Future collisions may depend on the future movement of other particles.
-        # Calculate self.collision_range steps ahead
-        collisions = [
-            self.check_collision(
-                current_distribution, locations + np.rint(directions * i).astype(int)
+            real_positions = np.where(collisions, positions, locations)
+            unique, counts = np.unique(real_positions, return_counts=True, axis=0)
+
+            current_distribution = np.copy(self.maze) * (
+                self.max_particles_per_cell + 1
             )
-            for i in range(1, self.collision_range)
-        ]
+            current_distribution[unique[:, 0], unique[:, 1]] = counts
+            current_distribution[
+                current_distribution <= self.max_particles_per_cell
+            ] = 1
+            current_distribution[current_distribution > self.max_particles_per_cell] = 0
 
-        self.collisions = np.max(np.concatenate(collisions, axis=1), axis=1,)[
-            :, np.newaxis
-        ]
+            new_collisions = self.check_collision(current_distribution, real_positions)
+
+            collisions = np.min(
+                np.concatenate([collisions, new_collisions], axis=1,), axis=1,
+            )[:, np.newaxis]
+            current_collisions = len(collisions) - collisions.sum()
+
+        # Particles which do not change their pixel should not have a collision.
+        # This prevents particles which enter a pixel and cause a collision
+        # from stopping particles (by marking them as having a collision) which
+        # have speed that is not sufficient to change the pixel
+        change = (
+            np.rint(self.exact_locations + self.particle_speed).astype(int) - locations
+        )
+        collisions = np.where(
+            np.sum(np.abs(change), axis=1)[:, np.newaxis] > 0, collisions, 1
+        )
+
+        self.collisions = collisions
         return self.collisions
 
     def step_done(self, valid_locations):
@@ -141,9 +164,14 @@ class PhysicalMovementModifier(StepModifier):
             self.exact_locations,
         )
         self.particle_speed = self.particle_speed / self.drag
+        # self.particle_speed = np.clip(self.particle_speed, 0.0, 1.0)
         self.particle_speed = np.where(
-            valid_locations, self.particle_speed, self.particle_speed / 2
+            valid_locations, self.particle_speed, self.particle_speed / 4
         )  # collision
-        self.particle_speed = np.where(
-            np.abs(self.particle_speed) > 0.01, self.particle_speed, 0
-        )  # stop really slow particles.
+
+        # stop really slow particles.
+        fast_particles = np.abs(self.particle_speed) > 0.01
+        self.particle_speed = np.where(fast_particles, self.particle_speed, 0)
+        self.exact_locations = np.where(
+            fast_particles, self.exact_locations, np.rint(self.exact_locations)
+        )
